1b28e241623499a47ec144cd7b08ad770b942b04
Toni Benitez <onfocoapp@gmail.com>
2025-03-15 21:27:01 +0100

    Cambiar CRLF por LF en VSCode

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
-
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Inject,
+  ParseUUIDPipe,
+  Query,
+  Patch,
+} from '@nestjs/common';
 
 import { NATS_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
@@ -9,10 +18,7 @@ import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
-
-  constructor(
-    @Inject(NATS_SERVICE) private readonly client: ClientProxy,
-  ) {}
+  constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post()
   create(@Body() createOrderDto: CreateOrderDto) {
@@ -20,19 +26,18 @@ export class OrdersController {
   }
 
   @Get()
-  findAll( @Query() orderPaginationDto: OrderPaginationDto ) {
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
     return this.client.send('findAllOrders', orderPaginationDto);
   }
-  
+
   @Get('id/:id')
-  async findOne(@Param('id', ParseUUIDPipe ) id: string) {
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
     try {
       const order = await firstValueFrom(
-        this.client.send('findOneOrder', { id })
+        this.client.send('findOneOrder', { id }),
       );
 
       return order;
-
     } catch (error) {
       throw new RpcException(error);
     }
@@ -44,30 +49,27 @@ export class OrdersController {
     @Query() paginationDto: PaginationDto,
   ) {
     try {
-
       return this.client.send('findAllOrders', {
         ...paginationDto,
         status: statusDto.status,
       });
-
     } catch (error) {
       throw new RpcException(error);
     }
   }
-
 
   @Patch(':id')
   changeStatus(
-    @Param('id', ParseUUIDPipe ) id: string,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() statusDto: StatusDto,
   ) {
     try {
-      return this.client.send('changeOrderStatus', { id, status: statusDto.status })
+      return this.client.send('changeOrderStatus', {
+        id,
+        status: statusDto.status,
+      });
     } catch (error) {
       throw new RpcException(error);
     }
   }
-
-
-
 }
